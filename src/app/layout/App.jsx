3d93f9d246b8bad2858afefcab8f9ac153d67f91
pavelklos <pavelkloscz@gmail.com>
2018-10-04 16:04:22 +0200

    Initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Re-vents</h1>
+      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h2 className="App-title">Welcome to Re-vents!</h2>
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+    );
+  }
+}
+
+export default App;
