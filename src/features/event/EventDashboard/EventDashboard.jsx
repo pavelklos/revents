1dfebed47563bf26d2163b1631b69d18ecf7151d
pavelklos <pavelkloscz@gmail.com>
2018-10-16 17:41:25 +0200

    End of Section 4

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Button } from "semantic-ui-react";
+import cuid from 'cuid';
 import EventList from "../EventList/EventList";
 import EventForm from "../EventForm/EventForm";
 
@@ -94,6 +95,16 @@ class EventDashboard extends Component {
     })
   }
 
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    const updatedEvents = [...this.state.events, newEvent];
+    this.setState({
+      events: updatedEvents,
+      isOpen: false
+    })
+  }
+
   render() {
     return (
       <Grid>
@@ -109,8 +120,9 @@ class EventDashboard extends Component {
           {/* <Button onClick={() => this.handleFormOpen('a string')} positive content="Create Event" /> */}
           <Button onClick={this.handleFormOpen} positive content="Create Event" />
           
-          {this.state.isOpen &&  
-          <EventForm handleCancel={this.handleCancel} />}
+          {this.state.isOpen && ( 
+            <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />
+          )}
         
         </Grid.Column>
       </Grid>
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -2,32 +2,69 @@ import React, { Component } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 
 class EventForm extends Component {
+
+  state = {
+    event: {
+      title: '',
+      date: '',
+      city: '',
+      venue: '',
+      hostedBy: ''
+    }
+  }
+
+  onFormSubmit = (evt) => {
+    evt.preventDefault();
+    // console.log(this.refs.title.value);
+    console.log(this.state.event);
+    this.props.createEvent(this.state.event)
+  }
+
+  onTitleChanged = (evt) => {
+    this.setState({
+      event: {
+        title: evt.target.value
+      }
+    })
+  }
+
+  // Generic Method for Input
+  onInputChanged = (evt) => {
+    const newEvent = this.state.event;
+    newEvent[evt.target.name] = evt.target.value
+    this.setState({
+      event: newEvent
+    })
+  }
+
   render() {
-
     const {handleCancel} = this.props;
-
+    const {event} = this.state;
     return (
       <Segment>
-        <Form>
+        <Form onSubmit={this.onFormSubmit} >
           <Form.Field>
             <label>Event Title</label>
-            <input placeholder="First Name" />
+            {/* <input placeholder="Event Title" /> */}
+            {/* <input ref='title' placeholder="Event Title" /> */}
+            {/* <input onChange={this.onTitleChanged} value={event.title} placeholder="Event Title" /> */}
+            <input name='title' onChange={this.onInputChanged} value={event.title} placeholder="Event Title" />
           </Form.Field>
           <Form.Field>
             <label>Event Date</label>
-            <input type="date" placeholder="Event Date" />
+            <input type="date" name='date' onChange={this.onInputChanged} value={event.date} placeholder="Event Date" />
           </Form.Field>
           <Form.Field>
             <label>City</label>
-            <input placeholder="City event is taking place" />
+            <input name='city' onChange={this.onInputChanged} value={event.city} placeholder="City event is taking place" />
           </Form.Field>
           <Form.Field>
             <label>Venue</label>
-            <input placeholder="Enter the Venue of the event" />
+            <input name='venue' onChange={this.onInputChanged} value={event.venue} placeholder="Enter the Venue of the event" />
           </Form.Field>
           <Form.Field>
             <label>Hosted By</label>
-            <input placeholder="Enter the name of person hosting" />
+            <input name='hostedBy' onChange={this.onInputChanged} value={event.hostedBy} placeholder="Enter the name of person hosting" />
           </Form.Field>
           <Button positive type="submit">
             Submit
